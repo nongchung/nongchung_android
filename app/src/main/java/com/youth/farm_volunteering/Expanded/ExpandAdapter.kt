0d34dda9b9f0299c7bd1387e88e9432eaeb8a5ba
kimseungki94 <skkim0832@gmail.com>
2018-07-06 01:26:04 +0900

    expanded listview 완료

--- app/src/main/java/com/youth/farm_volunteering/Expanded/ExpandAdapter.kt
+++ app/src/main/java/com/youth/farm_volunteering/Expanded/ExpandAdapter.kt
@@ -13,10 +13,10 @@ import com.youth.farm_volunteering.R
 import com.youth.farm_volunteering.R.id.tv_title
 import kotlinx.android.synthetic.main.layout_group.view.*
 
-class ExpandAdapter(var context : Context,var expandedView : ExpandableListView,var header : MutableList<String>, var body : MutableList<MutableList<String>>) : BaseExpandableListAdapter()  {
+class ExpandAdapter(var context: Context, var expandedView: ExpandableListView, var header: MutableList<String>, var body: MutableList<MutableList<String>>) : BaseExpandableListAdapter() {
     override fun getGroup(groupPosition: Int): String {
         return header[groupPosition]
-          }
+    }
 
     override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
         return true
@@ -35,41 +35,32 @@ class ExpandAdapter(var context : Context,var expandedView : ExpandableListView,
         val title = convertView!!.findViewById<TextView>(R.id.tv_title)
         title.text = getGroup(groupPosition)
         title?.setOnClickListener {
-            if(expandedView.isGroupExpanded(groupPosition)) {
+            if (expandedView.isGroupExpanded(groupPosition)) {
                 expandedView.collapseGroup(groupPosition)
-               // convertView!!.imageView.isSelected = false
-            }
-            else {
+                //convertView!!.imageView.isPressed = false
 
+            } else {
                 expandedView.expandGroup(groupPosition)
-               // convertView!!.imageView.isSelected = true
-
+                //convertView!!.imageView.isPressed = true
             }
-            Toast.makeText(context,getGroup(groupPosition),Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, getGroup(groupPosition), Toast.LENGTH_SHORT).show()
         }
         if (isExpanded) {
-            title.setTypeface(null, Typeface.BOLD);
-            convertView.imageView.isSelected = true
-
+            convertView.imageView.isPressed = true
         } else {
-            // If group is not expanded then change the text back into normal
-            // and change the icon
-
-            convertView.imageView.isSelected = false
-           title.setTypeface(null, Typeface.NORMAL);
-
+            convertView.imageView.isPressed = false
         }
         return convertView
     }
 
     override fun getChildrenCount(groupPosition: Int): Int {
-       return body[groupPosition].size
+        return body[groupPosition].size
 
     }
 
     override fun getChild(groupPosition: Int, childPosition: Int): String {
-            return body[groupPosition][childPosition]
-        }
+        return body[groupPosition][childPosition]
+    }
 
     override fun getGroupId(groupPosition: Int): Long {
         return groupPosition.toLong()
@@ -77,15 +68,21 @@ class ExpandAdapter(var context : Context,var expandedView : ExpandableListView,
 
     override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View? {
         var convertView = convertView
+
+
+
         if (convertView == null) {
+
             val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             convertView = inflater.inflate(R.layout.layout_child, null)
         }
         val title = convertView?.findViewById<TextView>(R.id.tv_title)
-        title?.text = getChild(groupPosition,childPosition)
+        title?.text = getChild(groupPosition, childPosition)
         title?.setOnClickListener {
-            Toast.makeText(context,getChild(groupPosition,childPosition),Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(context, getChild(groupPosition, childPosition), Toast.LENGTH_SHORT).show()
         }
+
         return convertView
     }
 
@@ -95,5 +92,5 @@ class ExpandAdapter(var context : Context,var expandedView : ExpandableListView,
 
     override fun getGroupCount(): Int {
         return header.size
-         }
+    }
 }
