c0421ad133d9e696e62204ab8d380d9da1fcb4c0
sungsoo <hudsucker@naver.com>
2018-07-10 11:31:35 +0900

    마이페이지로그아웃 추가

--- app/src/main/java/com/youth/farm_volunteering/MyPage/MypageFragment.kt
+++ app/src/main/java/com/youth/farm_volunteering/MyPage/MypageFragment.kt
@@ -75,7 +75,15 @@ class MypageFragment : Fragment() {
             var nick = Intent(this.context, NicknameFragment::class.java)
             startActivity(nick)
         })
+        v.layout_mypage_logout.setOnClickListener {
+            LoginToken.token = null;
+            var sharedPreference = activity.getSharedPreferences(LoginToken.PREF_KEY, Context.MODE_PRIVATE);
+            var editor = sharedPreference.edit();
+            editor.remove(LoginToken.PREF_KEY)
+            editor.commit()
+            Toast.makeText(activity!!, "로그아웃에 성공하였습니다.", Toast.LENGTH_SHORT).show()
 
+        }
 //        v.button_mypage_logout.setOnClickListener {
 //            LoginToken.token = null;
 //            var sharedPreference = activity.getSharedPreferences(LoginToken.PREF_KEY, Context.MODE_PRIVATE);
