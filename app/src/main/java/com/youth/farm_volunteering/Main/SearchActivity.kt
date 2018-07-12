e85006e75eef4d08b13ecf1ec4e0ad9b40d3fd52
sungsoo <hudsucker@naver.com>
2018-07-13 05:35:54 +0900

    검색만드는중

--- app/src/main/java/com/youth/farm_volunteering/Main/SearchActivity.kt
+++ app/src/main/java/com/youth/farm_volunteering/Main/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.youth.farm_volunteering.R
+import com.youth.farm_volunteering.area.AreaSelectActivity
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
