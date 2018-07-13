37668f46b7d774b35cc6d94906a1eb2a739e81a6
sungsoo <hudsucker@naver.com>
2018-07-13 20:18:29 +0900

    no message

--- app/src/main/java/com/youth/farm_volunteering/area/AreaSelectActivity.kt
+++ app/src/main/java/com/youth/farm_volunteering/area/AreaSelectActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.widget.GridLayoutManager
 import com.youth.farm_volunteering.Main.*
 import com.youth.farm_volunteering.R
+import com.youth.farm_volunteering.R.id.recyclerview_selectarea_area
+import kotlinx.android.synthetic.main.activity_selectarea.*
 import kotlinx.android.synthetic.main.activity_selectarea.view.*
 
 class AreaSelectActivity : AppCompatActivity() {
@@ -13,7 +16,7 @@ class AreaSelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selectarea)
-
+        recyclerview_selectarea_area.layoutManager = GridLayoutManager(this, 4)
 
     }
 }
