c5d07d1b4204749d89d04a6654f2c3af6c9de776
maeun <maeuniyee@naver.com>
2018-07-13 16:09:04 +0900

    메인에서 하트 눌러도 반응 없는 거 해결

--- app/src/main/java/com/youth/farm_volunteering/data/MyPageData.kt
+++ app/src/main/java/com/youth/farm_volunteering/data/MyPageData.kt
@@ -1,10 +1,8 @@
 package com.youth.farm_volunteering.data
 
 class MyPageData {
-    var startDate: String? = null
-    var endDate: String? = null
-    var addr: Int? = null
-    var period: String? = null
+    var img: String? = null
+    var mail: String? = null
     var name: String? = null
     var period: String? = null
 }
