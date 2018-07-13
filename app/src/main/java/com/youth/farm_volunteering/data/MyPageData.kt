5a01e01b9df553b6e3a3850c196470f108e158f9
maeun <maeuniyee@naver.com>
2018-07-13 15:40:18 +0900

    no message

--- app/src/main/java/com/youth/farm_volunteering/data/MyPageData.kt
+++ app/src/main/java/com/youth/farm_volunteering/data/MyPageData.kt
@@ -1,8 +1,10 @@
 package com.youth.farm_volunteering.data
 
 class MyPageData {
-    var mail: String? = null
+    var startDate: String? = null
+    var endDate: String? = null
+    var addr: Int? = null
+    var period: String? = null
     var name: String? = null
-    var point: Int? = null
-    var img: String? = null
+    var period: String? = null
 }
