5a09b6bbf9f64704f78386eb318989ff5188fdc9
kimseungki94 <skkim0832@gmail.com>
2018-08-06 01:15:41 +0900

    비밀번호 변경 구현

--- app/src/main/java/com/youth/farm_volunteering/MyPage/ChangePasswordActivity.kt
+++ app/src/main/java/com/youth/farm_volunteering/MyPage/ChangePasswordActivity.kt
@@ -3,15 +3,20 @@ package com.youth.farm_volunteering.MyPage
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.asksira.loopingviewpagerdemo.ApplicationController
 import com.youth.farm_volunteering.R
-import com.youth.farm_volunteering.R.id.toolbar
+import com.youth.farm_volunteering.R.id.*
 import com.youth.farm_volunteering.data.NickNameResponseData
 import com.youth.farm_volunteering.data.PasswordResourceData
+import kotlinx.android.synthetic.main.activity_change_password.*
+import kotlinx.android.synthetic.main.activity_signup1.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,6 +27,8 @@ class ChangePasswordActivity : AppCompatActivity() {
     var newpassword : String? = null
     lateinit var editText: EditText
     lateinit var neweditText: EditText
+    var passwordChangeData: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +39,23 @@ class ChangePasswordActivity : AppCompatActivity() {
         editText = findViewById(R.id.editText7)
         neweditText = findViewById(R.id.editText11)
 
+        textView88.visibility = View.INVISIBLE
+        imageView13.visibility = View.INVISIBLE
+
+//        editText11.addTextChangedListener(object : TextWatcher {
+//            override fun afterTextChanged(p0: Editable?) {
+//                if(editText7.text.toString() != p0.toString()){
+//                    textView88.visibility = View.VISIBLE
+//                    imageView13.visibility = View.VISIBLE
+//                }
+//                else if(editText7.text.toString() == p0.toString()){
+//                    textView88.visibility = View.INVISIBLE
+//                    imageView13.visibility = View.INVISIBLE
+//                }
+//            }
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+//        })
 
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -59,10 +83,20 @@ class ChangePasswordActivity : AppCompatActivity() {
 
                         when (response.code()) {
                             200 -> {
-                                Toast.makeText(this@ChangePasswordActivity, response.body().message, Toast.LENGTH_SHORT).show() // 메시지생성
-                            }
 
-                            else ->
+                                    intent.putExtra(passwordChangeData, editText11.text.toString())
+                                    Toast.makeText(this@ChangePasswordActivity, response.body().message, Toast.LENGTH_SHORT).show() // 메시지생성
+                                    finish()
+                            }
+                            400 -> {
+                                if (editText7.text.toString() == "") {
+                                    Toast.makeText(this@ChangePasswordActivity, "비밀번호를 다시 확인해주세요", Toast.LENGTH_SHORT).show()
+
+                                } else if (editText11.text.toString() == "") {
+                                    Toast.makeText(this@ChangePasswordActivity, "새 비밀번호를 다시 확인해주세요", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                            500 ->
                                 Toast.makeText(this@ChangePasswordActivity, response.body().message, Toast.LENGTH_SHORT).show()
                         }
                     }
